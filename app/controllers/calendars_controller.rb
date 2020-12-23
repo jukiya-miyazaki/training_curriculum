acf4c9e749399b108ff1c83f1a0906ac3f251614
jukiya-miyazaki <jm.iamu222@icloud.com>
2020-12-23 15:38:28 +0900

    日付が表示されるように修正
    Issue5

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,8 +2,8 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    get_week
-    plan = Plan.new
+    getWeek
+    @plan = Plan.new
   end
 
   # 予定の保存
@@ -18,11 +18,11 @@ class CalendarsController < ApplicationController
     params.require(:plan).permit(:date, :plan)
   end
 
-  def get_week
+  def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
-    @todays_date = Date.today.day
+    @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
@@ -34,14 +34,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-      wday_num = []# wdayメソッドを用いて取得した数値
-      if wday_num > wdays +7 #「wday_numが7以上の場合」という条件式
-      else
-        wday_num = wday_num -7
-      end
-
-      days = { :month => (@todays_date + x).month, :date => @todays_date.day + x, :plans => today_plans, :wday => wdays #wdaysから値を取り出す記述}
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
       @week_days.push(days)
     end
 
